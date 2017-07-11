15530d157275bbcf3644835bb683ba302a4e58c3
Sebastian Kruse <sebastian.kruse@hpi.de>
2017-07-11 17:52:42 +0200

    fix worker assignment in LoadAwareSchedulingStrategy

--- akka-tutorial/src/main/java/de/hpi/akka_tutorial/remote/actors/scheduling/LoadAwareSchedulingStrategy.java
+++ akka-tutorial/src/main/java/de/hpi/akka_tutorial/remote/actors/scheduling/LoadAwareSchedulingStrategy.java
@@ -1,19 +1,13 @@
 package de.hpi.akka_tutorial.remote.actors.scheduling;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.stream.Collectors;
-
 import akka.actor.ActorRef;
 import de.hpi.akka_tutorial.remote.actors.Worker;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class LoadAwareSchedulingStrategy implements SchedulingStrategy {
-	
+
 	/**
 	 * This class supervises the state of a range query for primes.
 	 */
@@ -27,13 +21,13 @@ public class LoadAwareSchedulingStrategy implements SchedulingStrategy {
 
 		// This is the ID of the query that is being tracked.
 		private final int id;
-		
+
 		// Keeps track of the currently posed subqueries and which actor is processing it.
 		private final Map<ActorRef, Worker.ValidationMessage> runningSubqueries = new HashMap<>();
 
 		// Keeps track of failed subqueries, so as to reschedule them to some worker.
 		private final Queue<Worker.ValidationMessage> failedSubqueries = new LinkedList<>();
-		
+
 		private QueryTracker(final int id, final long startNumber, final long endNumber) {
 			this.id = id;
 			this.remainingRangeStartNumber = startNumber;
@@ -46,7 +40,7 @@ public class LoadAwareSchedulingStrategy implements SchedulingStrategy {
 		 * @return a new subquery or {@code null}
 		 */
 		Worker.ValidationMessage pollNextSubquery(ActorRef actorRef) {
-			
+
 			// Restart a failed subquery if any
 			if (!this.failedSubqueries.isEmpty()) {
 				Worker.ValidationMessage subquery = this.failedSubqueries.poll();
@@ -100,7 +94,7 @@ public class LoadAwareSchedulingStrategy implements SchedulingStrategy {
 					&& this.remainingRangeStartNumber > this.remainingRangeEndNumber;
 		}
 	}
-	
+
 
 	// A mapping of pending range queries to the query tracker that watches the progress of each range query; the queries are kept in their insertion order
 	private final LinkedHashMap<Integer, QueryTracker> queryId2tracker = new LinkedHashMap<>();
@@ -110,33 +104,33 @@ public class LoadAwareSchedulingStrategy implements SchedulingStrategy {
 
 	// A reference to the actor in whose name we send messages
 	private final ActorRef master;
-	
+
 	public LoadAwareSchedulingStrategy(ActorRef master) {
 		this.master = master;
 	}
 
 	@Override
 	public void schedule(final int taskId, final long startNumber, final long endNumber) {
-		
+
 		// Create a new tracker for the query
 		QueryTracker tracker = new QueryTracker(taskId, startNumber, endNumber);
 		this.queryId2tracker.put(tracker.id, tracker);
 
 		// Assign existing, possible free, workers to the new query
-		this.assignSubqueries();		
+		this.assignSubqueries();
 	}
-	
+
 	@Override
 	public boolean hasTasksInProgress() {
 		return !this.queryId2tracker.isEmpty();
 	}
-	
+
 	@Override
 	public void finished(int taskId, ActorRef worker) {
-		
+
 		// Find the query being processed
 		QueryTracker queryTracker = this.queryId2tracker.get(taskId);
-		
+
 		// Mark the worker as free
 		queryTracker.workCompleted(worker);
 		this.worker2tracker.put(worker, null);
@@ -150,20 +144,20 @@ public class LoadAwareSchedulingStrategy implements SchedulingStrategy {
 			this.assignSubqueries();
 		}
 	}
-	
+
 	@Override
 	public void addWorker(final ActorRef worker) {
-		
+
 		// Add the new worker
 		this.worker2tracker.put(worker, null);
-		
+
 		// Assign possibly open subqueries to the new worker
 		this.assignSubqueries();
 	}
 
 	@Override
 	public void removeWorker(final ActorRef worker) {
-		
+
 		// Remove the worker from the list of workers
 		QueryTracker processedTracker = this.worker2tracker.remove(worker);
 
@@ -178,7 +172,7 @@ public class LoadAwareSchedulingStrategy implements SchedulingStrategy {
 
 	// Assign currently free workers to subqueries
 	private void assignSubqueries() {
-		
+
 		// Collect all currently idle workers
 		Collection<ActorRef> idleWorkers = this.worker2tracker.entrySet().stream()
 				.filter(e -> e.getValue() == null)
@@ -187,25 +181,24 @@ public class LoadAwareSchedulingStrategy implements SchedulingStrategy {
 
 		// Try to assign new subqueries to all idle workers
 		Iterator<QueryTracker> queryTrackerIterator = this.queryId2tracker.values().iterator();
+
+		// Check if there is any on-going query at the moment
+		if (!queryTrackerIterator.hasNext()) return;
+		QueryTracker nextQueryTracker = queryTrackerIterator.next();
+
+		// Assign all idle workers as long as there is work to do
 		for (ActorRef idleWorker : idleWorkers) {
-			while (true) {
-				// Return if no work is to be scheduled
-				if (!queryTrackerIterator.hasNext()) 
-					return;
-				
-				// Retrieve the next query tracker (older queries are picked first)
-				QueryTracker nextQueryTracker = queryTrackerIterator.next();
-				
-				// Poll the next open subquery from the query tracker; continue if no work is to be done
-				Worker.ValidationMessage subquery;
-				if ((subquery = nextQueryTracker.pollNextSubquery(idleWorker)) == null) {
-					continue;
-				}
-				
-				// Assign the subquery to the worker and keep track of the assignment
-				idleWorker.tell(subquery, this.master);
-				this.worker2tracker.put(idleWorker, nextQueryTracker);
+			// Poll the next subquery from the current query tracker
+			Worker.ValidationMessage subquery;
+			if ((subquery = nextQueryTracker.pollNextSubquery(idleWorker)) == null) {
+				// If the current query tracker does not have open work items, go to the next one
+				if (!queryTrackerIterator.hasNext()) return;
+				nextQueryTracker = queryTrackerIterator.next();
 			}
+
+			// Assign the subquery to the worker and keep track of the assignment
+			idleWorker.tell(subquery, this.master);
+			this.worker2tracker.put(idleWorker, nextQueryTracker);
 		}
 	}
 }
