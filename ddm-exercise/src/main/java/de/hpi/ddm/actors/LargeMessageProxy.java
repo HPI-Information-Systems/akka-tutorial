f0d564d244904f4d5a87e4520abcc9c2c1a0f14e
Felix Gohla <felix.gohla@student.hpi.de>
2021-05-19 21:22:16 +0200

    Allow sending and receiving multiple messages

--- ddm-exercise/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-exercise/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -4,6 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
@@ -12,7 +15,10 @@ import akka.actor.Props;
 import de.hpi.ddm.singletons.KryoPoolSingleton;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.experimental.Accessors;
 
 public class LargeMessageProxy extends AbstractLoggingActor {
 
@@ -21,8 +27,9 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////////
 
 	public static final String DEFAULT_NAME = "largeMessageProxy";
-	public static final int CHUNK_SIZE = 512;
-	
+	// Assumption: This is sent over TCP and Ethernet and the MTU is 1500.
+	public static final int CHUNK_SIZE = 1024;
+
 	public static Props props() {
 		return Props.create(LargeMessageProxy.class);
 	}
@@ -34,19 +41,27 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	// Messages to be sent to the sender's proxy
 	// =========================================
 
-	@Data @NoArgsConstructor @AllArgsConstructor
+	@Data @AllArgsConstructor
 	public static class LargeMessage<T> implements Serializable {
 		private static final long serialVersionUID = 2940665245810221108L;
-		private T message;
-		private ActorRef receiver;
+		private int id;
+		@NonNull private T message;
+		@NonNull ActorRef receiver;
+
+		LargeMessage(T message, ActorRef receiver) {
+			this.message = message;
+			this.receiver = receiver;
+			this.id = ThreadLocalRandom.current().nextInt();
+		}
 	}
 
 	/**
 	 * A message to be sent to the sender's proxy to ask it to send the next chunk to the receiver's proxy.
 	 */
-	@Data
-	public static class RequestNextChunkMessage implements Serializable {
+	@Data @NoArgsConstructor @AllArgsConstructor
+	private static class RequestNextChunkMessage implements Serializable {
 		private static final long serialVersionUID = -5374303325056263165L;
+		private int id;
 	}
 
 	// Messages to be sent to the receiver's proxy
@@ -56,18 +71,21 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	 * A message to be sent to the receiver's proxy to let it know it can start asking the sender's proxy
 	 * for the first chunk.
 	 */
-	@Data
-	public static class StartMessage implements Serializable {
+	@Data @NoArgsConstructor @AllArgsConstructor
+	private static class StartMessage implements Serializable {
 		private static final long serialVersionUID = 2746268654101845915L;
+		private int id;
+		private int numBytesSerialized;
 	}
 
 	/**
 	 * A message containing the next chunk.
 	 */
 	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class NextChunkMessage implements Serializable {
+	private static class NextChunkMessage implements Serializable {
 		private static final long serialVersionUID = 4057807743872319842L;
-		private byte[] bytes;
+		private int id;
+		private byte[] payload;
 	}
 
 	/**
@@ -77,11 +95,11 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	 * of the serialized message so that the receiver's proxy can verify it has received all the bytes.
 	 */
 	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class NoChunksLeftMessage implements Serializable {
+	private static class NoChunksLeftMessage implements Serializable {
 		private static final long serialVersionUID = -4373062859625501319L;
+		private int id;
 		private ActorRef sender;
 		private ActorRef receiver;
-		private int numBytesSer;
 	}
 
 	/////////////////
@@ -89,15 +107,30 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	/////////////////
 
 	// Fields of the sender's proxy
-	private byte[] serMsg;
-	private ActorRef sender, receiver;
-	private ActorSelection receiverProxy;
-	private int currChunkStartIx;
-	private boolean hasSentAllChunks;
+	@Data @NoArgsConstructor
+	class SendMessageState {
+		private byte[] serMsg;
+		private ActorRef sender, receiver;
+		private ActorSelection receiverProxy;
+		private int currChunkStartIx;
+		@Getter
+		@Accessors(fluent = true)
+		private boolean hasSentAllChunks;
+	}
 
 	// Fields of the receiver's proxy
-	private ByteArrayOutputStream baos;  // used for storing the bytes from the received chunks
-	private boolean hasSentDeserMsg;
+	@Data @NoArgsConstructor
+	class ReceiveMessageState {
+		private boolean started = false;
+		private ByteArrayOutputStream buffer; // used for storing the bytes from the received chunks
+		private int expectedBytes;
+		@Accessors(fluent = true)
+		private boolean hasSentDeserMsg;
+	}
+
+	HashMap<Integer, SendMessageState> sendMessageStates = new HashMap<Integer, SendMessageState>();
+	HashMap<Integer, ReceiveMessageState> receiveMessageStates = new HashMap<Integer, ReceiveMessageState>();
+
 
 	/////////////////////
 	// Actor Lifecycle //
@@ -111,7 +144,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Behavior //
 	////////////////////
-	
+
 	@Override
 	public Receive createReceive() {
 		return receiveBuilder()
@@ -125,7 +158,6 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	}
 
 	private void handle(LargeMessage<?> largeMessage) {
-
 		// TODO: Implement a protocol that transmits the potentially very large message object.
 		// The following code sends the entire message wrapped in a BytesMessage, which will definitely fail in a distributed setting if the message is large!
 		// Solution options:
@@ -140,59 +172,139 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		// - But: Good, language-dependent serializers (such as kryo) are aware of byte arrays so that their serialization is very effective w.r.t. serialization time and size of serialized data.
 		this.log().info("Received large message.");
 		Object message = largeMessage.getMessage();
+
+		SendMessageState ms = new SendMessageState();
 		// Save the references to the actual sender and receiver, as well as to the receiver's proxy into the fields
 		// of the sender's proxy.
-		this.sender = this.sender();
-		this.receiver = largeMessage.getReceiver();
-		this.receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
+		ms.setSender(this.sender());
+		ms.setReceiver(largeMessage.getReceiver());
+		ms.setReceiverProxy(this.context().actorSelection(ms.receiver.path().child(DEFAULT_NAME)));
 
 		// Serialize the message and initialize/reset the sender's proxy fields.
-		this.serMsg = KryoPoolSingleton.get().toBytesWithClass(message);
-		this.currChunkStartIx = 0;
-		this.hasSentAllChunks = false;
+		ms.setSerMsg(KryoPoolSingleton.get().toBytesWithClass(message));
+		ms.setCurrChunkStartIx(0);
+		ms.hasSentAllChunks(false);
 		// Tell the receiver's proxy it can start requesting chunks.
-		this.receiverProxy.tell(new LargeMessageProxy.StartMessage(), this.getSelf());
+		this.log().info("Sending start message for message with ID {}.", largeMessage.getId());
+		this.sendMessageStates.put(largeMessage.getId(), ms);
+		ms.getReceiverProxy().tell(new LargeMessageProxy.StartMessage(largeMessage.getId(), ms.getSerMsg().length), this.getSelf());
+
+
+		// TODO: set a timeout.
 	}
 
 	private void handle(StartMessage startMessage) {
-		this.baos = new ByteArrayOutputStream();
-		sender().tell(new RequestNextChunkMessage(), this.getSelf());
-	}
+		final int messageId = startMessage.getId();
+		this.log().info("Handling start message for receiving a large message with ID {}.", messageId);
+		// Check whether there is already a message with that particular ID.
+		if (this.receiveMessageStates.containsKey(messageId)) {
+			// TODO: throw;
+		}
 
-	private void handle(RequestNextChunkMessage nextChunkMessage) {
-		if (!this.hasSentAllChunks) {
-			// Get the next chunk and send it to the receiver's proxy.
-			int currChunkEndIx = Math.min(currChunkStartIx + CHUNK_SIZE, serMsg.length);
-			byte[] chunk = Arrays.copyOfRange(serMsg, currChunkStartIx, currChunkEndIx);
-			receiverProxy.tell(new NextChunkMessage(chunk), this.getSelf());
+		ReceiveMessageState state = new ReceiveMessageState();
+		state.setBuffer(new ByteArrayOutputStream(startMessage.getNumBytesSerialized()));
+		state.setExpectedBytes(startMessage.getNumBytesSerialized());
+		this.receiveMessageStates.put(messageId, state);
 
-			if (currChunkEndIx == this.serMsg.length) {
-				this.hasSentAllChunks = true;
-				this.log().info("All chunks sent, sending NoChunksLeftMessage to receiver's proxy {}...", receiverProxy);
-				receiverProxy.tell(new LargeMessageProxy.NoChunksLeftMessage(this.sender, this.receiver, this.serMsg.length), this.getSelf());
-			} else {
-				currChunkStartIx += CHUNK_SIZE;
-			}
+		this.log().info("Have {} messages in progress ({}).", this.receiveMessageStates.size(), this.receiveMessageStates.keySet().stream().map((i) -> i.toString()).reduce("", String::concat));
+
+		if (this.receiveMessageStates.size() < 3) {
+			state.setStarted(true);
+			this.log().info("Requesting first block of message with ID {}.", messageId);
+			sender().tell(new RequestNextChunkMessage(messageId), this.getSelf());
+		} else {
+			state.setStarted(false);
+			this.log().info("Deferring message with ID {}.", messageId);
 		}
 	}
 
+	private void handle(RequestNextChunkMessage nextChunkMessage) {
+		final int messageId = nextChunkMessage.getId();
+		SendMessageState state = this.sendMessageStates.get(messageId);
+		if (state == null) {
+			// TODO: throw;
+			return;
+		}
+
+		if (!state.hasSentAllChunks()) {
+			// Get the next chunk and send it to the receiver's proxy.
+			int currChunkEndIx = Math.min(state.getCurrChunkStartIx() + CHUNK_SIZE, state.getSerMsg().length);
+			byte[] chunk = Arrays.copyOfRange(state.getSerMsg(), state.getCurrChunkStartIx(), currChunkEndIx);
+			state.getReceiverProxy().tell(new NextChunkMessage(messageId, chunk), this.getSelf());
+
+			if (currChunkEndIx == state.getSerMsg().length) {
+				state.hasSentAllChunks(true);
+				this.log().info("All chunks sent for message with ID {}, sending NoChunksLeftMessage to receiver's proxy {}...", messageId, state.getReceiverProxy());
+				state.getReceiverProxy().tell(
+					new LargeMessageProxy.NoChunksLeftMessage(
+						messageId,
+						state.getSender(),
+						state.getReceiver()
+					),
+					this.getSelf()
+				);
+				this.sendMessageStates.remove(messageId);
+			} else {
+				state.setCurrChunkStartIx(state.getCurrChunkStartIx() + CHUNK_SIZE);
+			}
+		} else {
+			// TODO: Throw;
+		}
+	}
+
+	// Receiver side...
+
 	private void handle(NextChunkMessage message) throws IOException {
+		final int messageId = message.getId();
+		ReceiveMessageState state = this.receiveMessageStates.get(messageId);
+
+		if (state == null) {
+			// TODO: throw;
+			return;
+		}
+
 		// Save the bytes from the received chunk and request the next one.
-		baos.write(message.getBytes());
-		sender().tell(new RequestNextChunkMessage(), this.getSelf());
+		state.getBuffer().write(message.getPayload());
+		// this.log().info("Size of buffer: {}.", state.getBuffer().size());
+		sender().tell(new RequestNextChunkMessage(messageId), this.getSelf());
 	}
 
 	private void handle(NoChunksLeftMessage noChunksLeftMessage) throws Exception {
-		if (!hasSentDeserMsg) {
-			byte[] serMsg = baos.toByteArray();  // technically, `this.serMsg` also could be used here since its unset for the receiver's proxy
-			if (serMsg.length != noChunksLeftMessage.getNumBytesSer()) {
-				throw new Exception(String.format("Number of received bytes != number of bytes of serialized message [%d] != [%d]", serMsg.length, noChunksLeftMessage.numBytesSer));
+		final int messageId = noChunksLeftMessage.getId();
+		ReceiveMessageState state = this.receiveMessageStates.remove(messageId);
+
+		if (state == null) {
+			// TODO: throw;
+			return;
+		}
+
+		this.log().info("Received all chunks for large message with ID {}.", messageId);
+
+		if (!state.hasSentDeserMsg()) {
+			if (state.getBuffer().size() != state.expectedBytes) {
+				throw new Exception(String.format(
+					"Number of received bytes != number of bytes of serialized message [%d] != [%d]",
+					state.getBuffer().size(),
+					state.expectedBytes
+				));
 			}
 
-			Object deserMsg = KryoPoolSingleton.get().fromBytes(serMsg);
+			Object deserMsg = KryoPoolSingleton.get().fromBytes(state.getBuffer().toByteArray());
 			this.log().info("Deserialized message, sending it to actual receiver...");
 			noChunksLeftMessage.getReceiver().tell(deserMsg, noChunksLeftMessage.getSender());
-			hasSentDeserMsg = true;
+			state.hasSentDeserMsg(true);
+		}
+
+		// Start requesting packages for a message that hasn't been started.
+		Entry<Integer, ReceiveMessageState> nextStartedState = this.receiveMessageStates.entrySet()
+			.stream()
+			.filter((s) -> !s.getValue().isStarted())
+			.findFirst()
+			.orElse(null);
+		if (nextStartedState != null) {
+			this.log().info("Requesting deferred message with ID {}.", messageId);
+			nextStartedState.getValue().setStarted(true);
+			sender().tell(new RequestNextChunkMessage(nextStartedState.getKey()), this.getSelf());
 		}
 	}
 }
